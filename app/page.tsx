dae796e7e9f88a5982124eb9fd78a9f72a48d045
Gabriel Castro <gabrielcastromail@gmail.com>
2024-11-06 01:23:32 -0300

    chore: git hooks setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
-import { Button } from "./_components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import { dark } from "@clerk/themes";
 
 export default async function HomePage() {
   const { userId } = await auth();
