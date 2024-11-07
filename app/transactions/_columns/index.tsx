a7796e2e208fdfe9cdad03dd68e4bd2cf408d8a7
Gabriel Castro <gabrielcastromail@gmail.com>
2024-11-06 22:56:42 -0300

    feat: ensure transaction table labels and rendered property

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -3,6 +3,30 @@
 import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import { TransactionTypeBadge } from "../_components/typeBadge";
+import { Button } from "@/app/_components/ui/button";
+import { PencilIcon, TrashIcon } from "lucide-react";
+
+export const TRANSACTION_CATEGORY_LABELS = {
+  HOUSING: "Housing",
+  TRANSPORTATION: "Transportation",
+  FOOD: "Food",
+  ENTERTAINMENT: "Entertainment",
+  HEALTH: "Health",
+  UTILITY: "Utility",
+  SALARY: "Salary",
+  EDUCATION: "Education",
+  OTHER: "Other",
+};
+
+export const TRANSACTION_PAYMENTH_METHODS_LABELS = {
+  CREDIT_CARD: "Credit Card",
+  DEBIT_CARD: "Debit Card",
+  BANK_TRANSFER: "Bank Transfer",
+  BANK_SLIP: "Bank Slip",
+  CASH: "Cash",
+  PIX: "Pix",
+  OTHER: "Other",
+};
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -19,21 +43,49 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "category",
     header: "Category",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_CATEGORY_LABELS[transaction.category],
   },
   {
     accessorKey: "paymentMethod",
     header: "Payment Method",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_PAYMENTH_METHODS_LABELS[transaction.paymentMethod],
   },
   {
     accessorKey: "date",
     header: "Date",
+    cell: ({ row: { original: transaction } }) => (
+      <p className="text-muted-foreground">
+        {new Date(transaction.date).toLocaleDateString("pt-BR", {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        })}
+      </p>
+    ),
   },
   {
     accessorKey: "amount",
     header: "Amount",
+    cell: ({ row: { original: transaction } }) =>
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(Number(transaction.amount)),
   },
   {
     accessorKey: "actions",
     header: "",
+    cell: () => (
+      <div className="space-x-1">
+        <Button variant={"ghost"} className="text-muted-foreground">
+          <PencilIcon size={"icon"} />
+        </Button>
+        <Button variant={"ghost"} className="text-muted-foreground">
+          <TrashIcon size={"icon"} />
+        </Button>
+      </div>
+    ),
   },
 ];
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -12,7 +12,7 @@ export default async function TransactionsPage() {
     <div className="space-y-6 p-6">
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transactions</h1>
-        <Button className="rounded-full">
+        <Button className="rounded-full font-bold">
           Add Transaction <ArrowDownUpIcon className="ml-2" />
         </Button>
       </div>
