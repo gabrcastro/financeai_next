a8e5ac190ce03a5dce8805f704f698f6ffee2e3d
Gabriel Castro <gabrielcastromail@gmail.com>
2024-11-10 01:31:06 -0300

    feat: add stripe webhook for subscription

--- app/subscription/_components/cardPlan.tsx
+++ app/subscription/_components/cardPlan.tsx
@@ -5,12 +5,16 @@ import { Card, CardContent } from "@/app/_components/ui/card";
 import { Benefit } from "./benefit";
 import { createStripeCheckout } from "../_actions/createCheckout";
 import { loadStripe } from "@stripe/stripe-js";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 interface ICardPlan {
   plan: "basic" | "premium";
-  active?: boolean;
 }
-export function CardPlan({ plan, active }: ICardPlan) {
+export function CardPlan({ plan }: ICardPlan) {
+  const { user } = useUser();
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan === "premium";
+
   async function handleAcquirePlanClick() {
     const { sessionId } = await createStripeCheckout();
     if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
@@ -25,8 +29,13 @@ export function CardPlan({ plan, active }: ICardPlan) {
     await stripe.redirectToCheckout({ sessionId });
   }
 
+  if (!process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_LINK)
+    throw new Error("Please specify a valid portal link");
+
   return (
-    <Card className="col-span-1 p-0">
+    <Card
+      className={`col-span-1 p-0 ${((hasPremiumPlan && plan === "premium") || (!hasPremiumPlan && plan === "basic")) && "boder border-primary"}`}
+    >
       <CardContent className="relative flex-col space-y-6 px-0 py-6">
         <div className="relative flex flex-col">
           <div className="flex flex-col items-center justify-center space-y-3">
@@ -46,13 +55,26 @@ export function CardPlan({ plan, active }: ICardPlan) {
             <Benefit title={"Unlimited the AI"} exist={plan === "premium"} />
             <Benefit title={"..."} exist={plan === "premium"} />
           </div>
-          <Button
-            onClick={handleAcquirePlanClick}
-            variant={plan === "basic" ? "outline" : "default"}
-            className={`w-full rounded-full border-primary font-bold ${plan === "basic" ? "text-primary" : "text-muted"} ${active && "bg-primary/20 hover:cursor-not-allowed hover:bg-primary/20 hover:text-primary"}`}
-          >
-            {active ? "Active" : "Upgrade"}
-          </Button>
+          {plan === "premium" && hasPremiumPlan ? (
+            <Button
+              className={`w-full rounded-full border border-primary bg-transparent font-bold text-primary hover:bg-transparent hover:text-primary`}
+            >
+              <Link
+                href={`${process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_LINK}?prefilled_email=${user.emailAddresses[0].emailAddress}`}
+              >
+                Manage plan
+              </Link>
+            </Button>
+          ) : plan === "premium" && !hasPremiumPlan ? (
+            <Button
+              onClick={handleAcquirePlanClick}
+              className={`w-full rounded-full border border-primary font-bold text-muted`}
+            >
+              Upgrade
+            </Button>
+          ) : (
+            <></>
+          )}
         </div>
 
         {/* {active && (
